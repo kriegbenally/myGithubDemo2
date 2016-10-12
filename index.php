e0d82bf492f8a72751a92d901f76a49fc11f2da9
Bedonie <phelanzobedoniecc@gmail.com>
2016-10-12 11:09:28 -0600

    added link for stylesheet
    added link for stylesheet

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="lib/css/style.css">
   </head>
   <body>
 
     <h1>jakkie</h1>
     <h1>phelanzo</h1>
-    
+
   </body>
 </html>
